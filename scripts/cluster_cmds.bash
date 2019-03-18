21f8fb26781a43a6a9a5ae7ce0bd2a085ef477ec
simon-schaefer <s-h-schaefer@web.de>
2019-03-19 00:01:33 +0100

    qsub file

--- scripts/cluster_cmds.bash
+++ scripts/cluster_cmds.bash
@@ -11,6 +11,7 @@ nvidia-smi
 # Queue handling. 
 qstat 
 qdel -u sischaef
+qquota -u sischaef | grep max_gpu_per_user
 
 # Background calculation (https://www.rackaid.com/blog/linux-screen-tutorial-and-how-to/). 
 screen              # start session (CRTL+A+D to detach). 
@@ -18,7 +19,7 @@ screen -ls          # list running sessions.
 screen -r "id"      # reattach session. 
 
 ### Example Jobs ####
-# src/tar/main.py --template IM_AE_TAD_DIV2K --no_augment --verbose --data_range "1-100/1-10" --batch_size 5 --loss "HR*10*MSE+LR*1*L1" --print_every 10
+# src/tar/main.py --template IM_AE_TAD_DIV2K --no_augment --verbose --data_range "1-700/1-10" --loss "HR*10*MSE+LR*1*L1" --print_every 10 --gamma 0.99 --lr 1e-4
 # src/tar/main.py --template IM_AE_TAD_DIV2K --no_augment --verbose --data_range "3-4/3-4" --batch_size 1 --loss "HR*100*L1+LR*1*L1" --lr 1e-4
 # src/tar/main.py --template IM_AE_TAD_DIV2K_SMALL --no_augment --verbose --print_every 10
 # src/tar/main.py --template IM_AE_TAD_DIV2K --no_augment --verbose --print_every 10
---
+++ scripts/job.bash
@@ -0,0 +1,21 @@
+#!/bin/bash
+#
+## otherwise the default shell would be used 
+#$ -S /bin/bash
+#
+## <= 1h is short queue, <= 6h is middle queue, <= 48h is long queue
+#$ -q gpu.middle.q@*
+# 
+## the maximum memory usage of this job
+#$ -l gpu=1 -l h_vmem=8G
+# 
+## stderr and stdout are merged together to stdout 
+#$ -j y
+#
+# logging directory. preferrably on your scratch 
+#$ -o /scratch_net/biwidl215/sischaef/outs/
+# 
+# calling exectuable. 
+source /scratch_net/biwidl215/sischaef/tar/scripts/setup.bash
+source /scratch_net/biwidl215/sischaef/tar/scripts/cluster_setup.bash
+#python3 /scratch_net/biwidl215/sischaef/tar/src/tar/main.py --template IM_AE_TAD_DIV2K --no_augment --verbose --data_range 1-100/1-10 --batch_size 5 --loss HR*10*MSE+LR*1*L1 --print_every
