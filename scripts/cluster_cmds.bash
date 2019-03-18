c6ccdebd7d2d380acfaa750e7dac3fbacbe58b9b
simon-schaefer <s-h-schaefer@web.de>
2019-03-19 00:30:43 +0100

    qsub with argument

--- scripts/cluster_cmds.bash
+++ scripts/cluster_cmds.bash
@@ -19,10 +19,10 @@ screen -ls          # list running sessions.
 screen -r "id"      # reattach session. 
 
 ### Example Jobs ####
-# src/tar/main.py --template IM_AE_TAD_DIV2K --no_augment --verbose --data_range "1-700/1-10" --loss "HR*10*MSE+LR*1*L1" --print_every 10 --gamma 0.99 --lr 1e-4
-# src/tar/main.py --template IM_AE_TAD_DIV2K --no_augment --verbose --data_range "3-4/3-4" --batch_size 1 --loss "HR*100*L1+LR*1*L1" --lr 1e-4
-# src/tar/main.py --template IM_AE_TAD_DIV2K_SMALL --no_augment --verbose --print_every 10
-# src/tar/main.py --template IM_AE_TAD_DIV2K --no_augment --verbose --print_every 10
-# src/tar/main.py --template IM_AE_TAD_MNIST --no_augment
-# src/tar/main.py --template IM_AE_TAD_MNIST --data_range "1-20/21-25" --no_augment --verbose
-# src/tar/main.py --template IM_AE_TEST --no_augment
+# --template=IM_AE_TAD_DIV2K --no_augment --verbose --data_range=1-700/1-10 --loss=HR*10*MSE+LR*1*L1 --print_every=10 --gamma=0.99 --lr=1e-4
+# --template=IM_AE_TAD_DIV2K --no_augment --verbose --data_range=3-4/3-4 --batch_size=1 --loss=HR*100*L1+LR*1*L1 --lr=1e-4
+# --template=IM_AE_TAD_DIV2K_SMALL --no_augment --verbose --print_every=10
+# --template=IM_AE_TAD_DIV2K --no_augment --verbose --print_every=10
+# --template=IM_AE_TAD_MNIST --no_augment
+# --template=IM_AE_TAD_MNIST --data_range="1-20/21-25" --no_augment --verbose
+# --template=IM_AE_TEST --no_augment
--- scripts/job.bash
+++ scripts/job.bash
@@ -7,7 +7,7 @@
 #$ -q gpu.middle.q@*
 # 
 ## the maximum memory usage of this job
-#$ -l gpu=1 -l h_vmem=8G
+#$ -l gpu=1 -l h_vmem=20G
 # 
 ## stderr and stdout are merged together to stdout 
 #$ -j y
@@ -18,4 +18,4 @@
 # calling exectuable. 
 source /scratch_net/biwidl215/sischaef/tar/scripts/setup.bash
 source /scratch_net/biwidl215/sischaef/tar/scripts/cluster_setup.bash
-#python3 /scratch_net/biwidl215/sischaef/tar/src/tar/main.py --template IM_AE_TAD_DIV2K --no_augment --verbose --data_range 1-100/1-10 --batch_size 5 --loss HR*10*MSE+LR*1*L1 --print_every
+python3 /scratch_net/biwidl215/sischaef/tar/src/tar/main.py "$@" 
