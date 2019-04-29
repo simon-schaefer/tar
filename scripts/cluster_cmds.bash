f6c630681e7b4a8daecce1089bdcf217883da42e
simon-schaefer <sischaef@student.ethz.ch>
2019-04-29 14:21:42 +0200

    file downscaling debugging

--- scripts/cluster_cmds.bash
+++ scripts/cluster_cmds.bash
@@ -1,25 +1,26 @@
 #!/bin/bash
 
-# Login to cluster environment. 
+# Login to cluster environment.
 qrsh -l gpu=1 -l h_vmem=20G -q gpu.middle.q@*
 qrsh -l gpu=0 -q "*@biwirenderXX"
 
-# Check gpu and user. 
+# Check gpu and user.
 grep -h $(whoami) /tmp/lock-gpu*/info.txt
 nvidia-smi
 
-# Job submission. 
-qsub scripts/job.bash 
+# Job submission.
+qsub scripts/job24.bash
+qsub scripts/job48.bash 
 
-# Queue handling. 
-qstat 
+# Queue handling.
+qstat
 qdel -u sischaef
 qquota -u sischaef | grep max_gpu_per_user
 
-# Background calculation (https://www.rackaid.com/blog/linux-screen-tutorial-and-how-to/). 
-screen              # start session (CRTL+A+D to detach). 
-screen -ls          # list running sessions. 
-screen -r "id"      # reattach session. 
+# Background calculation (https://www.rackaid.com/blog/linux-screen-tutorial-and-how-to/).
+screen              # start session (CRTL+A+D to detach).
+screen -ls          # list running sessions.
+screen -r "id"      # reattach session.
 
 ### Example Jobs ####
 # --template=IM_AE_TAD_DIV2K --no_augment --verbose --data_range=1-700/1-10 --batch_size=6 --loss=HR*10*L1+LR*1*L1 --lr=1e-4 --gamma=0.99
--- src/tests/downsample_dataset.py
+++ src/tests/downsample_dataset.py
@@ -2,11 +2,11 @@
 # -*- coding: utf-8 -*-
 # =============================================================================
 # Created By  : Simon Schaefer
-# Description : Downsample images in given dataset for RGB images. For 
-#               grayscale input the channels are stacked, i.e. R=G=B. 
-# Arguments   : Path to HR images directory.  
-#               Scaling factor (int). 
-#               Path to LR images directory (optional). 
+# Description : Downsample images in given dataset for RGB images. For
+#               grayscale input the channels are stacked, i.e. R=G=B.
+# Arguments   : Path to HR images directory.
+#               Scaling factor (int).
+#               Path to LR images directory (optional).
 # =============================================================================
 import imageio
 import glob
@@ -15,9 +15,9 @@ import os
 import sys
 import skimage
 
-def progress_bar(iteration: int, num_steps: int, bar_length: int=50) -> int: 
-    """ Draws progress bar showing the number of executed 
-    iterations over the overall number of iterations. 
+def progress_bar(iteration: int, num_steps: int, bar_length: int=50) -> int:
+    """ Draws progress bar showing the number of executed
+    iterations over the overall number of iterations.
     Increments the iteration and returns it. """
     status = ""
     progress = float(iteration) / float(num_steps)
@@ -31,18 +31,18 @@ def progress_bar(iteration: int, num_steps: int, bar_length: int=50) -> int:
     sys.stdout.flush()
     return iteration + 1
 
-# Get directory of images from input arguments. 
-if not len(sys.argv) >= 3: 
+# Get directory of images from input arguments.
+if not len(sys.argv) >= 3:
     raise ValueError("Please state [directory, scale] !")
 directory = sys.argv[1]
 scale = int(sys.argv[2])
-if not directory[-1] == "/": 
+if not directory[-1] == "/":
     directory = directory + "/"
 directory_out = sys.argv[3] if len(sys.argv) > 3 else "LR_bicubic"
 assert os.path.isdir(directory)
 assert scale > 0 and scale % 2 == 0
 
-# Iterate over all files in directory, load, downsample and save them. 
+# Iterate over all files in directory, load, downsample and save them.
 directory_out = os.path.dirname(directory[:-1]) + "/" + directory_out
 os.makedirs(directory_out, exist_ok=True)
 print("Starting downscaling to {} ...".format(directory_out))
@@ -50,20 +50,23 @@ image_files = glob.glob(directory + "*.png")
 num_files   = len(image_files)
 for i, filepath in enumerate(image_files):
     hr = imageio.imread(filepath)
-    if len(hr.shape) == 2: 
+    if len(hr.shape) == 2:
         hr = np.stack((hr,hr,hr), axis=2)
-    if not hr.shape[0] % 2 == 0: 
+    if not hr.shape[0] % 2 == 0:
         hr = hr[:-1,:,:]
-    if not hr.shape[1] % 2 == 0: 
+    if not hr.shape[1] % 2 == 0:
         hr = hr[:,:-1,:]
     ldim = None
-    lr = skimage.transform.rescale(hr, 1.0/scale,  
-            anti_aliasing=True, 
-            multichannel=True, 
-            mode='reflect', 
-            preserve_range=True, 
+    lr = skimage.transform.rescale(hr, 1.0/scale,
+            anti_aliasing=True,
+            multichannel=True,
+            mode='reflect',
+            preserve_range=True,
             order=4
     )
+    print(filepath, hr.shape, lr.shape)
+    assert hr.shape[0] == scale*lr.shape[0]
+    assert hr.shape[1] == scale*lr.shape[1]
     filename, _ = os.path.splitext(os.path.basename(filepath))
     filename = directory_out + "/{}x{}.png".format(filename, scale)
     imageio.imwrite(filename, lr.astype(np.uint8))
