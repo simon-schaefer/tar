36edb8aac5cb66a81cde442e16c66a22ea381808
simon-schaefer <sischaef@student.ethz.ch>
2019-05-16 16:45:58 +0200

    simplified code

--- src/datasets/check.py
+++ src/datasets/check.py
@@ -13,21 +13,7 @@ import numpy as np
 import os
 import sys
 
-def progress_bar(iteration: int, num_steps: int, bar_length: int=50) -> int:
-    """ Draws progress bar showing the number of executed
-    iterations over the overall number of iterations.
-    Increments the iteration and returns it. """
-    status = ""
-    progress = float(iteration) / float(num_steps)
-    if progress >= 1.0:
-        progress, status = 1.0, "\r\n"
-    block = int(round(bar_length * progress))
-    text = "\r[{}] {:.0f}% {}".format(
-        "#" * block + "-" * (bar_length - block), round(progress*100, 0),
-        status)
-    sys.stdout.write(text)
-    sys.stdout.flush()
-    return iteration + 1
+from utils import progress_bar
 
 # Get directory of images from input arguments.
 if not len(sys.argv) >= 2:
