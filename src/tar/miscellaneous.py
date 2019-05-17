fb8ce9a9aa5a90398255bbe4d642b1c178025da5
simon-schaefer <sischaef@student.ethz.ch>
2019-05-17 13:54:59 +0200

    flow psnr debugging

--- src/tar/miscellaneous.py
+++ src/tar/miscellaneous.py
@@ -290,9 +290,9 @@ def convert_flow_to_color(flow):
     hsv = np.zeros((flow.shape[0],flow.shape[1],3), dtype=np.uint8)
     hsv[:,:,1] = 255
     mag, ang = cv2.cartToPolar(flow[...,0], flow[...,1])
-    hsv[:,:,0] = ang*180/np.pi/2
+    hsv[:,:,0] = 255
     hsv[:,:,2] = cv2.normalize(mag,None,0,255,cv2.NORM_MINMAX)
-    return cv2.cvtColor(hsv,cv2.COLOR_HSV2BGR)
+    return cv2.cvtColor(hsv,cv2.COLOR_HSV2BGR)/255.0
 
 def is_power2(num):
     return num != 0 and ((num & (num - 1)) == 0)
