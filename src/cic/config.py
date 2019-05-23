d9d81c054e0ea65b48f4695875ca5cac77694f7d
simon-schaefer <sischaef@student.ethz.ch>
2019-05-23 19:43:55 +0200

    colorful image colorization

---
+++ src/cic/config.py
@@ -0,0 +1,52 @@
+import os, sys
+
+path = os.path.split(__file__)[0]
+# print("abs path is %s" %(os.path.abspath()))
+
+config = {
+    'batch_size' : 32,
+    'val_batch_size':2,
+    'num_iters':1000000000,
+    'seed' : 1,
+    'lr':3.16e-4,
+
+    'lr_update_step':37500,
+    'test_cycle':10,
+
+    'cuda' : True,
+    'gpus' :1,
+    'gpuargs' : {'num_workers': 4,
+               'pin_memory' : True
+              },
+
+    'model':'ColorizationNet',
+    'bachnorm':True,
+    'pretrained':False,
+
+    # 'opt_config':{
+    #     'lr' : 0.001,
+    #     'betas' : (0.9, 0.99),
+    #     'eps': 1e-8,
+    #     'weight_decay': 0.004
+    # },
+
+    'save' :'%s/work/' % path,
+
+    'image_folder_train' : {
+        'root' : '%s/' % path,
+        'file' : os.path.join(os.environ["SR_PROJECT_DATA_PATH"], "DIV2K/DIV2K_train_HR/*.png") ,
+        'replicates': 1,
+        'train':True
+    },
+    'image_folder_val' : {
+        'root' : '%s/' % path,
+        'file' : os.path.join(os.environ["SR_PROJECT_DATA_PATH"], "DIV2K/DIV2K_train_HR/*.png"),
+        'replicates': 1,
+        'train':False
+    },
+
+    'log_frequency': 1, #frequency for the number of epotch
+    'save_iamge':'%s/work/img/' % path
+}
+
+# print(config['save'])
