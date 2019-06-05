dd638827b84f12cc668d37d19ecc982670d9b15b
simon-schaefer <sischaef@student.ethz.ch>
2019-06-05 10:21:23 +0200

    outs to dataset resolved

--- src/datasets/out_to_dataset.py
+++ src/datasets/out_to_dataset.py
@@ -25,17 +25,24 @@ assert os.path.isdir(directory)
 out_dir = os.environ["SR_PROJECT_DATA_PATH"]
 out_dir = os.path.join(out_dir,directory.split("/")[-1].replace("results_",""))
 out_dir = out_dir + "_SR"
+dset_name = os.path.basename(out_dir)
 out_hr, out_lr = os.path.join(out_dir,"HR"), os.path.join(out_dir,"LR_bicubic")
 os.makedirs(out_hr, exist_ok=True)
 os.makedirs(out_lr, exist_ok=True)
-fnames = []
-for fname in glob.glob(os.path.join(directory, "*.png")):
-    if fname.count(hr_tag)>0: fnames.append(os.path.basename(fname).split("_")[0])
 scales = []
 for fname in glob.glob(os.path.join(directory, "*.png")):
+    if os.path.basename(fname).count("EPS") > 0: continue 
     sc_tag = int(os.path.basename(fname).split("_")[-2].replace("x",""))
     if not sc_tag in scales: scales.append(sc_tag)
 for sc in scales: os.makedirs(os.path.join(out_lr, "X"+str(sc)),exist_ok=True)
+print(scales)
+fnames = []
+for fname in glob.glob(os.path.join(directory, "*.png")):
+    if fname.count("_"+hr_tag+".png")>0:
+        name = os.path.basename(fname).replace("_"+hr_tag+".png","")
+        for scale in scales: name = name.replace("_x" + str(scale), "")
+        fnames.append(name)
+print(fnames)
 
 # Copy files in fnames to out directory.
 for fname in fnames:
@@ -46,3 +53,4 @@ for fname in fnames:
         lr_src = os.path.join(directory, fname+"_x"+str(sc)+"_"+lr_tag+".png")
         lr_dst = os.path.join(out_lr, "X"+str(sc), fname+"x"+str(sc)+".png")
         shutil.copy2(lr_src, lr_dst)
+print("... successfully created {} dataset !".format(dset_name))
