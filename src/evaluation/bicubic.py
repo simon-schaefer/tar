a480eec949541e137cc5d0b4ab830ee27dbc89a5
Simon Schaefer <sischaef@student.ethz.ch>
2019-06-06 15:30:09 +0200

    evaluation -> bicubic for videos

--- src/evaluation/bicubic.py
+++ src/evaluation/bicubic.py
@@ -8,32 +8,40 @@
 # =============================================================================
 import argparse
 import imageio
+import glob
 import numpy as np
 import os
 import skimage
 
 # Get directory of images from input arguments.
 parser = argparse.ArgumentParser(description="bicubic")
-parser.add_argument("--lr", type=str, default="")
+parser.add_argument("--directory", type=str, default="")
+parser.add_argument("--tag", type=str, default="")
 parser.add_argument("--scale", type=int, default=4)
 args = parser.parse_args()
-assert os.path.isfile(args.lr)
+assert os.path.isdir(args.directory)
 assert args.scale > 0 and args.scale % 2 == 0
 
+# Load images from directory.
+lrs = glob.glob(args.directory + "/*.png")
+if args.tag != "": lrs = [x for x in lrs if x.count(args.tag) > 0]
+out = "{}_x{}b".format(os.path.basename(args.directory), args.scale)
+out = os.path.join(os.environ["SR_PROJECT_OUTS_PATH"], out)
+os.makedirs(out)
+
 # Load image, upscale it and save it.
-print("Bilinearly upscaling {} ...".format(path_LR))
-lr = imageio.imread(args.lr)
-lr = lr[:,:,:3]
-hr = skimage.transform.rescale(lr, args.scale,
-        anti_aliasing=True,
-        multichannel=True,
-        mode='reflect',
-        preserve_range=True,
-        order=3
-)
-filename, _ = os.path.splitext(os.path.basename(args.lr))
-filename    = filename.replace(".png","")+"_x{}b.png".format(str(args.scale))
-dirname = os.path.dirname(args.lr)
-path_HR = os.path.join(dirname,filename)
-imageio.imwrite(path_HR, hr.astype(np.uint8))
+print("Bilinearly upscaling {} ...".format(os.path.basename(args.directory)))
+for fname_lr in lrs:
+    lr = imageio.imread(fname_lr)
+    lr = lr[:,:,:3]
+    hr = skimage.transform.rescale(lr, args.scale,
+            anti_aliasing=True,
+            multichannel=True,
+            mode='reflect',
+            preserve_range=True,
+            order=3
+    )
+    filename, _ = os.path.splitext(os.path.basename(fname_lr))
+    filename    = filename.replace(".png","")+"_x{}b.png".format(str(args.scale))
+    imageio.imwrite(os.path.join(out,filename), hr.astype(np.uint8))
 print("... finished bilinear upscaling !")
--- src/evaluation/imgs_to_video.py
+++ src/evaluation/imgs_to_video.py
@@ -19,17 +19,17 @@ import matplotlib.pyplot as plt
 
 # Read input arguments.
 parser = argparse.ArgumentParser(description="imgs_to_video")
-parser.add_argument("--path", type=str, default="")
+parser.add_argument("--directory", type=str, default="")
 parser.add_argument("--video_name", type=str, default="video.avi")
 parser.add_argument("--tag", type=str, default="")
 args = parser.parse_args()
-assert os.path.isdir(args.path)
+assert os.path.isdir(args.directory)
 tag = "_" + args.tag if args.tag != "" else None
 
 # Make list of images in img_dir having the tag.
-images = glob.glob(args.path + "/*" + ".png")
-#for x in range(0, len(images)): images[x] = path_img_dir+"/hr"+str(x)+"x4.png"
-images = sorted(images)
+images = glob.glob(args.directory + "/*" + ".png")
+for x in range(0, len(images)): images[x] = args.directory+"/hr"+str(x)+"x4_x4b.png"
+#images = sorted(images)
 if tag is not None: images = [x for x in images if x.count(tag) > 0]
 
 # Create video using OpenCV functionalities.
@@ -37,6 +37,8 @@ fps = 10
 frame = cv2.imread(images[0])
 height, width, _ = frame.shape
 video = cv2.VideoWriter(args.video_name, 0, fps, (width,height))
-for image in images: video.write(cv2.imread(image))
+for image in images:
+    print(image)
+    video.write(cv2.imread(image))
 cv2.destroyAllWindows()
 video.release()
